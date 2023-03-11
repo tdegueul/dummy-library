64043f60662b8e1e95dcfb7ab28bb261a92f5ec8
Thomas Degueule <thomas.degueule@labri.fr>
2023-03-11 17:25:27 +0100

    Initial version of the calculator

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,21 @@
+package calculator;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int substract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public float divide(int a, int b) {
+        if (b == 0)
+            throw new IllegalArgumentException("division by zero");
+        return a / (float) b;
+    }
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,57 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CalculatorTest {
+    Calculator calc = new Calculator();
+
+    @Test
+    void add_two_numbers() {
+        assertThat(calc.add(2, 3), is(equalTo(5)));
+    }
+
+    @Test
+    void add_two_numbers_negative() {
+        assertThat(calc.add(2, -3), is(equalTo(-1)));
+    }
+
+    @Test
+    void substract_two_numbers() {
+        assertThat(calc.substract(3, 2), is(equalTo(1)));
+    }
+
+    @Test
+    void substract_two_numbers_negative() {
+        assertThat(calc.substract(2, 3), is(equalTo(-1)));
+    }
+
+    @Test
+    void multiply_two_numbers() {
+        assertThat(calc.multiply(2, 3), is(equalTo(6)));
+    }
+
+    @Test
+    void multiply_two_numbers_negative() {
+        assertThat(calc.multiply(2, -3), is(equalTo(-6)));
+    }
+
+    @Test
+    void divide_two_numbers() {
+        assertThat(calc.divide(6, 2), is(equalTo(3f)));
+    }
+
+    @Test
+    void divide_two_numbers_decimal() {
+        assertThat(calc.divide(6, 4), is(equalTo(1.5f)));
+    }
+
+    @Test
+    void div_two_numbers_zero() {
+        assertThrows(IllegalArgumentException.class, () -> calc.divide(2, 0));
+    }
+}
